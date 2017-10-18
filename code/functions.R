a536e8fda39180f23d6e7ef2e05f78e18031c0df
Marissa Lee <marissaruthlee@gmail.com>
2017-10-18 17:10:33 +1100

    fixed fxn

--- code/functions.R
+++ code/functions.R
@@ -23,14 +23,15 @@ LoadHarvestFiles<-function(){
 
 
 CalcTotalDryMass<-function(data){
-  data$dryMassCalc <- NA
-  x <- which(data$drill == 'no'); data$dryMassCalc[x] <- data$dryMass[x]
-  x <- which(data$drill == 'yes'); data$dryMassCalc[x] <- (data$wetWeight[x] * data$dryMass[x]) / data$drilledWeight[x]
+  data$totalSampleDryMass <- NA
+  x <- which(data$drill == 'no'); data$dtotalSampleDryMass[x] <- data$dryMass[x]
+  x <- which(data$drill == 'yes'); data$totalSampleDryMass[x] <- (data$wetWeight[x] * data$dryMass[x]) / data$drilledWeight[x]
   return(data)
 }
 
 CalcDensity<-function(data){
-  data$densityCalc[x] <- data$drilledWright[x] / data$volMass[x]
+  data$density <- NA
+  data$density <- data$drilledWeight / as.numeric(data$volMass)
   return(data)
 }
 
@@ -39,16 +40,19 @@ ReorgDataFrame<-function(data){
   require(tidyr)
   
   #add a species column
-  data1<-separate(data, unique, into=c("species","extraCode"), 4, remove=FALSE)
+  data1<-separate(data, unique, into=c("Species","extraCode"), 4, remove=FALSE)
   
   #add a size column
   data1$size<-NA
   data1[tolower(data1$species) == data1$species,"size"]<-"small"
   data1[tolower(data1$species) != data1$species,"size"]<-"large"
   
-  #organize columns
-  data2<-data1[,c("unique","species","size","time","totalSampleDryMass","density","fruitingBodies","insectDamage","drill")]
+  #rename
+  data2<-rename(data1, "fruiting"="fruitingBodies","insects"="insectDamage")
   
-  return(data2)
+  #organize columns
+  data3<-data2[,c("unique","Species","size","time","totalSampleDryMass","density","fruiting","insects","drill")]
+  
+  return(data3)
   
 }
