9763f01e7893fc41796236b36017348c23c6b217
Marissa Lee <marissaruthlee@gmail.com>
2017-10-24 11:17:31 +1100

    temporary fix to NaN in mass loss data

--- code/load_fxns.R
+++ code/load_fxns.R
@@ -37,77 +37,63 @@ process_initial_file<-function(df,size){
   return(df_out)
 }
 
-LoadHarvestFiles<-function(){
+read.samp1 <- function(){
+  samp1 <- read.csv('data/samp1data_201402.csv', stringsAsFactors=F)
+  samp1$notes <- ''
+  samp1$typesInsects <- ''
+  samp1$weightForVol <- samp1$dryMass
+  samp1$wetWeightForMass <- apply(samp1[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
+  samp1[is.na(samp1$wetWeight), 'wetWeightForMass'] <- NA
+  samp1$time <- 7
+  return(samp1)
+}
+
+read.samp2 <- function(){
+  samp2 <- read.csv('data/samp2data_201408.csv', stringsAsFactors=F)
+  samp2$typesInsects <- ''
+  samp2$weightForVol <- samp2$dryMass
+  samp2$wetWeightForMass <- apply(samp2[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
+  samp2[is.na(samp2$wetWeight), 'wetWeightForMass'] <- NA
+  samp2$time <- 13
+  return(samp2)
+}
+
+read.samp3 <- function(){
+  samp3 <- read.csv('data/samp3data_201508.csv', stringsAsFactors=F)
+  samp3$typesInsects <- ''
+  temp <- strsplit(samp3$wetWeightExcess, '+', fixed=T)
+  samp3$wetWeightExcess <- sapply(temp, function(x){if(length(x) == 2) sum(as.numeric(x)) else if(length(x) == 1) as.numeric(x) else NA})
+  samp3$weightForVol <- samp3$dryMass
+  samp3$wetWeightForMass <- apply(samp3[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
+  samp3[is.na(samp3$wetWeight), 'wetWeightForMass'] <- NA
+  samp3$time <- 25
+  samp3$volMass <- as.numeric(samp3$volMass) # this needs to be removed after fixing this column for time 3
+  return(samp3)
+}
+
+read.samp4 <- function(s3){
+  samp4 <- read.csv('data/samp4data_201608_quantitative.csv', stringsAsFactors=F)
+  names(samp4) <- gsub('wetWeightExcess..g.', 'wetWeightExcess', names(samp4))
+  temp <- strsplit(gsub('^\\(', '', samp4$drilledWeight), ' total) ', fixed=T)
+  samp4$weightForVol <- sapply(temp, function(x){if(length(x) == 2) as.numeric(x)[2] else as.numeric(x)[1]})
+  samp4[samp4$weightForVol == 0, 'weightForVol'] <- samp4[samp4$weightForVol == 0, 'wetWeight']
+  samp4$wetWeightForMass <- apply(samp4[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
+  samp4[is.na(samp4$wetWeight), 'wetWeightForMass'] <- NA
+  samp4[is.na(samp4$total.dry), 'total.dry'] <- samp4[is.na(samp4$total.dry), 'dryMass.piece.used.to.do.vol.mass.']
+  samp4$dryMass <- apply(samp4[, c('dry.WWE', 'total.dry')], 1, sum)
+  samp4.1 <- read.csv('data/samp4data_201608_qualitative.csv', stringsAsFactor=F)
+  names(samp4.1) <- gsub('notes', 'notes1', names(samp4.1))
+  samp4 <- merge(samp4, samp4.1)
+  samp4$notes <- with(samp4, paste(notes, notes1, sep=' -- '))
+  samp4$notes1 <- samp4$dry.WWE <- samp4$dryMass.piece.used.to.do.vol.mass. <- NULL
+  samp4$time <- 37
+
+  samp4<-samp4[, names(s3)]
   
-  # functions for data cleaning
-  read.samp1 <- function(){
-    samp1 <- read.csv('data/samp1data_201402.csv', stringsAsFactors=F)
-    samp1$notes <- ''
-    samp1$typesInsects <- ''
-    samp1$weightForVol <- samp1$dryMass
-    samp1$wetWeightForMass <- apply(samp1[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
-    samp1[is.na(samp1$wetWeight), 'wetWeightForMass'] <- NA
-    samp1$time <- 7
-    return(samp1)
-  }
-  
-  read.samp2 <- function(){
-    samp2 <- read.csv('data/samp2data_201408.csv', stringsAsFactors=F)
-    samp2$typesInsects <- ''
-    samp2$weightForVol <- samp2$dryMass
-    samp2$wetWeightForMass <- apply(samp2[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
-    samp2[is.na(samp2$wetWeight), 'wetWeightForMass'] <- NA
-    samp2$time <- 13
-    return(samp2)
-  }
-  
-  read.samp3 <- function(){
-    samp3 <- read.csv('data/samp3data_201508.csv', stringsAsFactors=F)
-    samp3$typesInsects <- ''
-    temp <- strsplit(samp3$wetWeightExcess, '+', fixed=T)
-    samp3$wetWeightExcess <- sapply(temp, function(x){if(length(x) == 2) sum(as.numeric(x)) else if(length(x) == 1) as.numeric(x) else NA})
-    samp3$weightForVol <- samp3$dryMass
-    samp3$wetWeightForMass <- apply(samp3[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
-    samp3[is.na(samp3$wetWeight), 'wetWeightForMass'] <- NA
-    samp3$time <- 25
-    samp3$volMass <- as.numeric(samp3$volMass) # this needs to be removed after fixing this column for time 3
-    return(samp3)
-  }
-  
-  read.samp4 <- function(){
-    samp4 <- read.csv('data/samp4data_201608_quantitative.csv', stringsAsFactors=F)
-    names(samp4) <- gsub('wetWeightExcess..g.', 'wetWeightExcess', names(samp4))
-    temp <- strsplit(gsub('^\\(', '', samp4$drilledWeight), ' total) ', fixed=T)
-    samp4$weightForVol <- sapply(temp, function(x){if(length(x) == 2) as.numeric(x)[2] else as.numeric(x)[1]})
-    samp4[samp4$weightForVol == 0, 'weightForVol'] <- samp4[samp4$weightForVol == 0, 'wetWeight']
-    samp4$wetWeightForMass <- apply(samp4[, c('wetWeight', 'wetWeightExcess')], 1, sum, na.rm=T)
-    samp4[is.na(samp4$wetWeight), 'wetWeightForMass'] <- NA
-    samp4[is.na(samp4$total.dry), 'total.dry'] <- samp4[is.na(samp4$total.dry), 'dryMass.piece.used.to.do.vol.mass.']
-    samp4$dryMass <- apply(samp4[, c('dry.WWE', 'total.dry')], 1, sum)
-    samp4.1 <- read.csv('data/samp4data_201608_qualitative.csv', stringsAsFactor=F)
-    names(samp4.1) <- gsub('notes', 'notes1', names(samp4.1))
-    samp4 <- merge(samp4, samp4.1)
-    samp4$notes <- with(samp4, paste(notes, notes1, sep=' -- '))
-    samp4$notes1 <- samp4$dry.WWE <- samp4$dryMass.piece.used.to.do.vol.mass. <- NULL
-    samp4$time <- 37
-    return(samp4[, names(s3)])
-  }
-  
-  # load data
-  s1 <- read.samp1()
-  s2 <- read.samp2()
-  s3 <- read.samp3()
-  s4 <- read.samp4()
-  
-  #bind everything together
-  s.data<-rbind(s1,s2,s3,s4)
-  
-  return(s.data)
-  
+  return(samp4)
 }
 
 CalcTotalDryMass<-function(data){
-  
   data$totalSampleDryMass <- NA
   x <- which(data$drill == 'no'); data$totalSampleDryMass[x] <- data$dryMass[x]
   x <- which(data$drill == 'yes'); data$totalSampleDryMass[x] <- (data$weightForVol[x] * data$dryMass[x]) / data$wetWeightForMass[x]
@@ -141,6 +127,37 @@ ReorgDataFrame<-function(data){
   return(data3)
   
 }
+
+LoadHarvestFiles<-function(){
+  
+  # load data
+  s1 <- read.samp1()
+  s2 <- read.samp2()
+  s3 <- read.samp3()
+  s4 <- read.samp4(s3)
+  
+  #bind everything together
+  s.data<-rbind(s1,s2,s3,s4)
+  filter(s.data, drill=="yes" & wetWeight == 0) #ripi1j is drilled, has a wetWeight ==0 and wetWeightExcess==0
+  
+  #calculate total dry mass
+  s.data<-CalcTotalDryMass(s.data)
+
+  #calculate density
+  s.data<-CalcDensity(s.data)
+  
+  #reorganize data frame
+  s.data<-ReorgDataFrame(s.data)
+  
+  #check for missing data
+  filter(s.data, is.na(totalSampleDryMass)) #see issue with ripi1j above
+  s.data<-s.data[s.data$unique!="ripi1j",] #exclude ripi1j for now
+  
+  return(s.data)
+  
+}
+
+
 
 
 #############
@@ -396,7 +413,6 @@ add_oomycetes<-function(fung.otu){
   data.df<-data.frame(seqSamp=row.names(data.otu), data.otu)
   
   #make mat.otu of fungal taxa a dataframe
-  fung.otu<-mat.otu
   fung.df<-data.frame(seqSamp=row.names(fung.otu), fung.otu)
   
   #merge by seqSamp
@@ -469,22 +485,18 @@ plot_sampleEffortCurves<-function(mat.otu){
 
 ##############
 
-make_ggplot_theme <- function(){
-  
-  #my ggplot template
-  mytheme <- theme_bw(base_size = 10, base_family = "Helvetica") +
-    theme(panel.border = element_rect(colour = "black"),      #put a black box around the plotting area
-          axis.line = element_line(colour = "black"),                 #axis lines are in black
-          panel.grid.major = element_blank(),                         #turn off the gridlines
-          panel.grid.minor = element_blank(),
-          strip.text.x = element_text(face='bold.italic', hjust=0.05),         #turn off the x axis facet labels
-          strip.text.y = element_text(face='bold.italic', hjust=0.05),
-          strip.background = element_rect(fill = 'white', colour='black'),    #make y axis facet labels be italic and top justified
-          legend.key = element_blank(),                               #turn off box around legend
-          plot.title=element_text(hjust=0, vjust=0.5, face='bold'), #style and position of the panel label
-          plot.margin = unit(c(0.05,0.05,0.05,0.05),"in")
-    )
-  
-  return(mytheme)
-}
-
+#ggplot theme
+mytheme <- theme_bw(base_size = 10, base_family = "Helvetica") +
+  theme(panel.border = element_rect(colour = "black"),      #put a black box around the plotting area
+        axis.line = element_line(colour = "black"),                 #axis lines are in black
+        panel.grid.major = element_blank(),                         #turn off the gridlines
+        panel.grid.minor = element_blank(),
+        strip.text.x = element_text(face='bold.italic', hjust=0.05),         #turn off the x axis facet labels
+        strip.text.y = element_text(face='bold.italic', hjust=0.05),
+        strip.background = element_rect(fill = 'white', colour='black'),    #make y axis facet labels be italic and top justified
+        legend.key = element_blank(),                               #turn off box around legend
+        plot.title=element_text(hjust=0, vjust=0.5, face='bold'), #style and position of the panel label
+        plot.margin = unit(c(0.05,0.05,0.05,0.05),"in")
+  )
+        
+        
