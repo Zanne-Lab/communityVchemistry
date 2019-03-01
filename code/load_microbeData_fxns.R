45d5e6656e63e640e38feea5830a235569ca71c9
Marissa Lee <marissaruthlee@gmail.com>
2019-03-01 12:18:01 -0500

    fixed OTU and taxon table loading functions

--- code/load_microbeData_fxns.R
+++ code/load_microbeData_fxns.R
@@ -1,5 +1,5 @@
 #stem sample meta data
-load_stemSamples<-function(){
+load_stemSamples <- function(){
   
   deployment <- read_csv("data/deployment.csv")
   deployment <- rename(deployment, "code"="species") #Code column
@@ -26,82 +26,34 @@ load_stemSamples<-function(){
 
 }
 
-#OTU table
-# load_matotu<-function(){
-#   
-#   # read in OTU table (uclust output) and convert to matrix (rows=samples, columns=OTUs)
-#   data.otu <- read.csv('data/sequencing_T0/DP16_OTUtable.csv', stringsAsFactors = FALSE)
-#   mat.otu <- as.matrix(data.otu[, 2:ncol(data.otu)]); rownames(mat.otu) <- data.otu[, 1]
-#   
-#   sum(colSums(mat.otu)==0) # if 0, then there are no empty columns
-#   
-#   # read in dataframe that contains sample information (also used to create meta and xrf)
-#   data <- read.csv('data/sequencing_T0/NextGenSeqencing_2016_Sample_MasterSpreadsheet.csv', stringsAsFactors=F)
-#   
-#   # extract 'blank' and 'mock' samples from 'mat.otu', delete from 'data'
-#   data <- data[!data$SampleCode == 'blank', ]
-#   
-#   # re-label rownames in 'mat.otu' with sample codes
-#   rownames(mat.otu) <- gsub('.', '-', rownames(mat.otu), fixed=T)
-#   rownames(mat.otu)[match(data$NextGenID, rownames(mat.otu))] <- data$SampleCode
-#   
-#   # extract 'blank' and 'mock' samples from 'mat.otu', delete from 'data'
-#   blank <- mat.otu[grep('blank', rownames(mat.otu)), ]
-#   mock <- mat.otu['mock', ]
-#   mat.otu <- mat.otu[-c(grep('blank', rownames(mat.otu)), grep('mock', rownames(mat.otu))), ]
-#   
-#   # otus, taxa in mock (select cut-off of >=9 reads in a sample)
-#   tax <-read.delim('data/sequencing_T0/DP16_tax.txt', stringsAsFactors = F)
-#   mock <- data.frame(reads=sort(mock[mock > 0]))
-#   mock <- cbind(mock, tax[match(rownames(mock), tax$OTU), 'species'])
-#   #mock
-#   mat.otu[mat.otu < 9] <- 0
-#   
-#   # otus, taxa in blank
-#   blank <- data.frame(reads=sort(blank[blank > 0]))
-#   blank <- cbind(blank, tax[match(rownames(blank), tax$OTU), 'species'])
-#   #mat.otu[,'ITSall_OTUd_3713'] # the most abundant OTU in the blank does not show up in any of the samples
-#   
-#   # re-order rows in 'mat.otu' to match rows in 'data'
-#   mat.otu <- mat.otu[match(data$SampleCode, rownames(mat.otu)), ]
-#   all(rownames(mat.otu) == data$SampleCode)  # is TRUE
-#   
-#   #how many Glomeromycota are there?
-#   GlomOTUs<-tax[tax$phylum=="Glomeromycota","OTU"]
-#   glom.mat.otu<-mat.otu[,colnames(mat.otu) %in% GlomOTUs]
-#   rowSums(glom.mat.otu)!=0
-#   tmp<-glom.mat.otu[,colSums(glom.mat.otu)!=0] #get rid of cols and rows without reads
-#   glom.mat.otu.sel<-tmp[rowSums(tmp)!=0,]
-#   
-#   return(mat.otu)
-#   
-# }
+add_oomycetes <- function(fung.otu){
 
-# add_oomycetes<-function(fung.otu){
-#   
-#   # read in OTU table (uclust output) and convert to matrix (rows=samples, columns=OTUs)
-#   data.otu <- read.csv('data/sequencing_T0/OTUtable_oomycetes_20171020.csv', row.names=1)
-#   data.df<-data.frame(seqSamp=row.names(data.otu), data.otu)
-#   
-#   #make mat.otu of fungal taxa a dataframe
-#   fung.df<-data.frame(seqSamp=row.names(fung.otu), fung.otu)
-#   
-#   #merge by seqSamp
-#   comm.df<-left_join(fung.df, data.df)
-#   
-#   #make NAs into 0s
-#   comm.df[is.na(comm.df)]<-0
-#   
-#   #make dataframe into a matrix again
-#   row.names(comm.df)<-comm.df$seqSamp
-#   comm.mat<-comm.df[,-1]
-#   comm.mat<-as.matrix(comm.mat)
-#   
-#   return(comm.mat)
-# }
+  # read in OTU table (uclust output) and convert to matrix (rows=samples, columns=OTUs)
+  data.otu <- read.csv('data/sequencing_T0/OTUtable_oomycetes_20171020.csv', row.names=1)
+  data.df<-data.frame(seqSamp=row.names(data.otu), data.otu)
+
+  #make mat.otu of fungal taxa a dataframe
+  fung.df<-data.frame(seqSamp=row.names(fung.otu), fung.otu)
+  
+  #merge by seqSamp
+  comm.df<-left_join(fung.df, data.df)
+
+  #make NAs into 0s
+  comm.df[is.na(comm.df)]<-0
+
+  #make dataframe into a matrix again
+  row.names(comm.df)<-comm.df$seqSamp
+  comm.mat<-comm.df[,-1]
+  comm.mat<-as.matrix(comm.mat)
+
+  #dim(fung.otu)
+  #dim(comm.mat) #check that there are more columns for oomycete OTUs
+  
+  return(comm.mat)
+}
 
 #copied from fungal_wood_endophtyes repo
-load_matotu<-function(){
+load_matotu <- function(){
   
   # read in OTU table (uclust output) and convert to matrix (rows=samples, columns=OTUs)
   data.otu <- read.csv('data/sequencing_T0/DP16_OTUtable.csv', stringsAsFactors = FALSE)
@@ -143,7 +95,8 @@ load_matotu<-function(){
   all(rownames(mat.otu) == data$SampleCode)  # is TRUE
   
   # add oomycetes
-  #mat.otu <- add_oomycetes(mat.otu)
+  mat.otu <- add_oomycetes(fung.otu = mat.otu)
+  sum(grepl("ITSoo", colnames(mat.otu)) == T) # verify that 13 oomycete OTUs have been added
   
   #trim OTUs that do not show up in ANY of the samples
   x <- apply(mat.otu > 0, 2, sum)
@@ -200,26 +153,32 @@ load_TaxAndFunguild <- function(comm.otu.tmp){
     }
   }
   
-  
-  # delete OTUs from taxAndFunguild if not found in comm.otu (only found in blanks, mock, or very infrequently)
-  # also delete OTUs with suspect coverage (probably nonfungal)
-  #hist(taxAndFunguild$coverage)
-  taxAndFunguild <- filter(taxAndFunguild, OTUId %in% colnames(comm.otu.tmp) & coverage > 0.9)
-  #hist(taxAndFunguild$coverage)
-  
-  # delete OTUs from comm.otu not found in taxAndFunguild (probably plant DNA)
-  #hist(rowSums(comm.otu))
-  comm.otu.tmp <- comm.otu.tmp[, colnames(comm.otu.tmp) %in% taxAndFunguild$OTUId]
-  # hist(rowSums(comm.otu))
-  
-  # # create a kingdom column
-  # taxAndFunguild$kingdom<-NA
-  # taxAndFunguild[grepl("Fungi", taxAndFunguild$taxonomy),"kingdom"]<-"Fungi"
+  # currently there is not oomycete information in the taxAndFunguild table
+  sum(grepl("ITSoo", taxAndFunguild$OTUId))
   
   # add oomycete OTUs as rows
-  #ooOTUs<-colnames(comm.otu)[!colnames(comm.otu) %in% taxAndFunguild$OTUId]
-  #oo.df<-data.frame(OTUId=ooOTUs, kingdom="Protist")
-  #taxAndFunguild<-bind_rows(taxAndFunguild, oo.df)
+  ooOTUs <- colnames(comm.otu.tmp)[grepl("ITSoo", colnames(comm.otu.tmp))]
+  oo.df<-data.frame(OTUId=ooOTUs, kingdom="Protist")
+  taxAndFunguild<-bind_rows(taxAndFunguild, oo.df)
+  
+  # identify OTUs from comm.otu not found in taxAndFunguild (probably plant DNA)
+  taxAndFunguild %>%
+    filter(is.na(coverage)) # the oomycetes don't have coverage data; everything else should
+  taxAndFunguild %>%
+    filter(kingdom != "Protist") %>%
+    filter(!OTUId %in% colnames(comm.otu.tmp)) %>%
+    select(OTUId, kingdom) -> nonfungalOTUs.df
+  taxAndFunguild %>%
+    filter(!OTUId %in% nonfungalOTUs.df$OTUId) -> tmp
+  # also delete OTUs with suspect coverage (probably nonfungal)
+  tmp %>%
+    filter(coverage > 0.9) -> tmp2
+  taxAndFunguild <- tmp2
+
+  # delete OTUs from taxAndFunguild if not found in comm.otu (only found in blanks, mock, or very infrequently)
+  tmp <- comm.otu.tmp[, colnames(comm.otu.tmp) %in% taxAndFunguild$OTUId]
+  dim(comm.otu.tmp); dim(tmp) # gets rid of more than 700 OTUs
+  comm.otu.tmp <- tmp
   
   # reorder taxAndFunguild to make OTU table
   o<-match(colnames(comm.otu.tmp), taxAndFunguild$OTUId)
@@ -243,18 +202,18 @@ load_TaxAndFunguild <- function(comm.otu.tmp){
   #unique(o.taxAndFunguild$Trophic.Mode)
   
   #clean Guild
-  #unique(o.taxAndFunguild$Guild)
+  unique(o.taxAndFunguild$Guild)
   o.taxAndFunguild[o.taxAndFunguild$Guild=="NULL","Guild"]<-"unclassified"
   
   #clean oomycetes
-  #o.taxAndFunguild[o.taxAndFunguild$kingdom=="Protist", c("taxonomy","phylum","family","genus")]<-"unclassified"
-  #o.taxAndFunguild[o.taxAndFunguild$kingdom=="Protist", c("species")]<-"unclassified_Protist"
+  o.taxAndFunguild[o.taxAndFunguild$kingdom=="Protist", c("taxonomy","phylum","family","genus")]<-"unclassified"
+  o.taxAndFunguild[o.taxAndFunguild$kingdom=="Protist", c("species")]<-"unclassified_Protist"
   
   #clean taxa
   o.taxAndFunguild %>%
     filter(is.na(genus)) %>%
     filter(Trophic.Mode != "unclassified") -> tmp
-  dim(tmp) # what were these matched in FUNGuild?  I'm going to remove the FUNGuild designation
+  dim(tmp) # why were these matched in FUNGuild?  I'm going to remove the FUNGuild designation
   o.taxAndFunguild[is.na(o.taxAndFunguild$phylum), 'phylum'] <- 'unclassified'
   o.taxAndFunguild[is.na(o.taxAndFunguild$family), 'family'] <- 'unclassified'
   o.taxAndFunguild[is.na(o.taxAndFunguild$genus), 'genus'] <- 'unclassified'
@@ -309,10 +268,8 @@ load_TaxAndFunguild <- function(comm.otu.tmp){
 #copied from fungal_wood_endophtyes repo
 clean_comm<-function(comm.otu.tmp, taxAndFunguild){
   
-  # delete OTUs from comm.otu not found in taxAndFunguild (probably plant DNA)
-  # hist(rowSums(comm.otu))
+  # delete OTUs from comm.otu not found in taxAndFunguild
   comm.otu <- comm.otu.tmp[, colnames(comm.otu.tmp) %in% taxAndFunguild$OTUId]
-  # hist(rowSums(comm.otu))
   
   return(comm.otu)
 }
@@ -337,56 +294,37 @@ load_seqSamples<-function(mat.otu, stemSamples){
   code.indx <- unique(stemSamples[,c("code","species","size")])
   code.indx %>%
     left_join(seqSamples.tmp) -> seqSamples
-  
+
   return(seqSamples)
 }
 
-# #taxon lookup info
-# load_TaxAndFunguild <- function(comm.otu) {
-#   require(dplyr)
-#   
-#   # load fungal OTU info
-#   funguild <-read.delim('data/sequencing_T0/DP16_funguild.txt', stringsAsFactors = F)
-#   tax <-read.delim('data/sequencing_T0/DP16_tax.txt', stringsAsFactors = F)
-#   
-#   # merge the dataframes by OTUId
-#   colnames(tax)[1] <- "OTUId" #make this match the column name in funguild
-#   taxAndFunguild <- left_join(tax, funguild)
-#   
-#   # create a kingdom column
-#   taxAndFunguild$kingdom<-NA
-#   taxAndFunguild[grepl("Fungi", taxAndFunguild$taxonomy),"kingdom"]<-"Fungi"
-#   
-#   # add oomycete OTUs as rows
-#   ooOTUs<-colnames(comm.otu)[!colnames(comm.otu) %in% taxAndFunguild$OTUId]
-#   oo.df<-data.frame(OTUId=ooOTUs, kingdom="Protist")
-#   taxAndFunguild<-bind_rows(taxAndFunguild, oo.df)
-#   
-#   # reorder taxAndFunguild to make OTU table
-#   o<-match(colnames(comm.otu), taxAndFunguild$OTUId)
-#   o.taxAndFunguild<-taxAndFunguild[o,]
-#   sum(o.taxAndFunguild$OTUId != colnames(comm.otu)) #this need to be 0
-#   
-#   # only use FUNGuild info with confidence ranking of Probable or Highly Probable
-#   o.taxAndFunguild[!o.taxAndFunguild$Confidence.Ranking %in% c("Probable","Highly Probable"),c("Trophic.Mode","Guild")]<-"unclassified"
-#   
-#   # select cols 
-#   o.taxAndFunguild %>%
-#     select(OTUId, taxonomy, kingdom, phylum, genus, species, 
-#            Trophic.Mode, Guild) -> o.taxAndFunguild
-#   
-#   #clean Trophic.Mode
-#   #unique(o.taxAndFunguild$Trophic.Mode)
-#   
-#   #clean Guild
-#   #unique(o.taxAndFunguild$Guild)
-#   o.taxAndFunguild[o.taxAndFunguild$Guild=="NULL","Guild"]<-"unclassified"
-#   
-#   #clean oomycetes
-#   o.taxAndFunguild[o.taxAndFunguild$kingdom=="Protist", c("taxonomy","phylum","genus","species")]<-"unclassified"
-#   
-#   return(o.taxAndFunguild)
-# }
+load_MicrobeCollection <- function(stemSamples){
+  
+  # OTU table
+  comm.otu <- load_matotu()
+  dim(comm.otu) # 3795 OTUs, includes oomycetes
+  
+  # taxon table
+  taxAndFunguild <- load_TaxAndFunguild(comm.otu)
+  dim(taxAndFunguild) # 3021 OTUs because removed OTUs with suspect coverage, non-fungal?
+  
+  # cleaning
+  comm.otu <- clean_comm(comm.otu, taxAndFunguild) # need to also remove these OTUs from the OTU table
+  dim(comm.otu) # good. also 3021 OTUs
+  
+  # sequencing sample look up table
+  seqSamples <- load_seqSamples(comm.otu, stemSamples) #create sequence sample meta data table
+  dim(seqSamples) # 105 samples (1 failed sequencing)
+  seqSamples[!seqSamples$seq_sampName %in% row.names(comm.otu),] # failed sample
+  
+  microb.data <- list(taxAndFunguild = taxAndFunguild,
+       comm.otu = comm.otu,
+       seqSamples = seqSamples)
+  
+  return(microb.data)
+  
+}
+
 
 #OTU sample-effort curves
 plot_sampleEffortCurves<-function(mat.otu){
